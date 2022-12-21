0cfaff57130da1260e2c0ae692b65bafc05a329e
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2022-12-21 10:27:47 +0000

    Create data_sources.tf

---
+++ data_sources.tf
@@ -0,0 +1,25 @@
+data "azurerm_log_analytics_workspace" "example" {
+  name                = "vmlogs"
+  resource_group_name = "Hub-RG"
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_subnet" "snet" {
+  name                 = var.subnet_name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_storage_account" "storeacc" {
+  count               = var.storage_account_name != null ? 1 : 0
+  name                = var.storage_account_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
